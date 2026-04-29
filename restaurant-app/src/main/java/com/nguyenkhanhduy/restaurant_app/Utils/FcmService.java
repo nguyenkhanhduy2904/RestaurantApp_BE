a497f3bad9a2df1be9700f071d3cf6668b9fc94f
KhanhDuy <nguyenkhanhduy.29042004tv@gmail.com>
2026-04-29 11:11:17 +0700

    fix some typo

--- restaurant-app/src/main/java/com/nguyenkhanhduy/restaurant_app/Utils/FcmService.java
+++ restaurant-app/src/main/java/com/nguyenkhanhduy/restaurant_app/Utils/FcmService.java
@@ -75,16 +75,11 @@ public class FcmService {
         sendMessage(
                 fcmToken,
                 new Notification("ORDER_UPDATE", "Your order have been updated"),
-                Map.of("type", "ORDER_UPDATED")
+                Map.of(
+                        "title", "Order Updated",
+                        "body", "Your Order have been updated",
+                        "type", "ORDER_UPDATED"
+                )
         );
-//        sendMessage(
-//                fcmToken,
-//                null,
-//                Map.of(
-//                        "title", "Order Updated",
-//                        "body", "Your Order have been updated",
-//                        "type", "ORDER_UPDATED"
-//                )
-//        );
     }
 }
